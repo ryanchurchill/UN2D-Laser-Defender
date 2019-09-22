afc4d88f3a1719f1fd2190dc49d8eabae0f31e6d
Ryan Churchill <ryan.churchill@gmail.com>
2019-09-22 16:02:08 -0700

    88 Movement and DeltaTime

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float deltaX = Input.GetAxis("Horizontal") *  Time.deltaTime * moveSpeed;
+        float deltaY = Input.GetAxis("Vertical") *  Time.deltaTime * moveSpeed;
+        float newXPos = transform.position.x + deltaX;
+        float newYPos = transform.position.y + deltaY;
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
